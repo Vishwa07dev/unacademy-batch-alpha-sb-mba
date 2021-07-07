1197a599fc4f20e84ce3a9ccca9ae5a31a0654fd
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2021-07-08 00:09:56 +0530

    added the code

---
+++ MovieBookingSystemApplication.java
@@ -0,0 +1,15 @@
+package com.vishwa.movieBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingSystemApplication.class, args);
+
+		System.out.println("Hello Spring Boot !!! ");
+	}
+
+}
